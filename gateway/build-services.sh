e0d7f1143f82eae630658c2411ac158e0004f6c2
Brad <brad.podmore@integration.works>
2022-12-20 14:36:04 +1300

    Added gateway

---
+++ gateway/build-services.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+result=0
+correct=200
+
+while [ "$result" -ne "$correct" ]
+do
+    result=`curl -s -o /dev/null -w "%{http_code}" http://kong:8001/`
+    sleep 1
+done
+
+curl -i -X POST http://kong:8001/services \
+    -H "Content-Type: application/json" \
+    -d '{ "port": 80, "protocol": "http", "name": "UsersAPIService", "path": "/api/Users", "host": "api"}'
+     
+curl -i -X POST http://kong:8001/services/UsersAPIService/routes \
+    -H "Content-Type: application/json" \
+    -d '{ "name": "UsersAPIRoute", "protocols": [ "http", "https" ], "paths": [ "/api/users" ] }'
