8d94704108d6933d66f8b489c64304bc404c858e
Brad <brad.podmore@integration.works>
2023-01-11 14:06:21 +1300

    Metrics

--- api/UserAPI/MetricReporting/RequestMetricReporter.cs
+++ api/UserAPI/MetricReporting/RequestMetricReporter.cs
@@ -4,23 +4,33 @@ namespace UserAPI.MetricReporting;
 
 public class RequestMetricReporter : IRequestMetricReporter
 {
-    private readonly Counter _requestCounter;
+    private readonly Counter _totalRequestCounter;
     private readonly Histogram _responseTimeHistogram;
+    private readonly Summary _statusCodeSummary;
 
     public RequestMetricReporter()
     {
-        _requestCounter = Metrics.CreateCounter("total_requests", "The total number of requests serviced by this API.");
+        _totalRequestCounter = Metrics.CreateCounter("total_requests", "The total number of requests serviced by this API.");
 
         _responseTimeHistogram = Metrics.CreateHistogram("request_duration_seconds", "The duration in seconds between the response to a request.", new HistogramConfiguration
         {
             Buckets = Histogram.ExponentialBuckets(0.01, 2, 10),
             LabelNames = new string[] { "status_code", "method" }
         });
+
+        _statusCodeSummary = Metrics.CreateSummary("response_code_summary", "A summary of all response codes created by the application.", new SummaryConfiguration
+        {
+            LabelNames = new string[] { "status_code" },
+        });
+
     }
 
     public void RegisterRequest(int code)
     {
-        _requestCounter.Inc();
+        _totalRequestCounter.Inc();
+        _statusCodeSummary
+            .WithLabels(code.ToString())
+            .Observe(1);
     }
 
     public void RegisterResponseTime(int statusCode, string method, TimeSpan elapsed)
